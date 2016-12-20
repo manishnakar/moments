cc86bb41b30e84bb7d73a3f930358c50fc343eda
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-20 14:31:15 -0800

    basic react setup finished

---
+++ app/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const App = require('./components/app');
+
+ReactDOM.render(<App />, document.getElementById('app'));
