4d19e6bf90f2dd973e5275c71cf347fba1795fb5
Stefanie Hansen <stefuhnee@gmail.com>
2017-01-07 19:15:18 -0800

    updated test

--- test/components/app-test.js
+++ test/components/app-test.js
@@ -8,8 +8,8 @@ import App from '../../src/components/App';
 describe('<App />', function(){
   const wrapper = shallow(<App />);
 
-  it('should render a Gallery', () => {
-    expect(wrapper.find('Gallery')).to.have.length(1);
+  it('should render a Galleres Container', () => {
+    expect(wrapper.find('Galleries')).to.have.length(1);
   });
 
   it('should render an Upload form', () => {
