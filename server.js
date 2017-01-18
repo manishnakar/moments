2aa2b2112e242796ded1f5685f7018dd4ba63d3f
Stefanie Hansen <stefuhnee@gmail.com>
2017-01-17 19:50:24 -0800

    server working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.listen(PORT, function() {
   console.log(`Listening on Port ${PORT}`);
