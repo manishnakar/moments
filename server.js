bdf5016bc0a7552e7d899b15b9fe38c8f78695b3
Stefanie Hansen <stefuhnee@gmail.com>
2017-01-17 19:52:29 -0800

    making post route for uploading photos

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
+app.post('/api/photo', function(req, res) {
+  
+});
+
 app.listen(PORT, function() {
   console.log(`Listening on Port ${PORT}`);
 });
